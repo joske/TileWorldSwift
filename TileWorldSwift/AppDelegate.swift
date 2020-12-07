3e87c964e7ea0ddc3a669750af6e834762b01e70
Jos Dehaes <jos.dehaes@gmail.com>
2020-12-07 20:39:02 +0100

    Initial Commit

---
+++ TileWorldSwift/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  TileWorldSwift
+//
+//  Created by Jos Dehaes on 07/12/2020.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
